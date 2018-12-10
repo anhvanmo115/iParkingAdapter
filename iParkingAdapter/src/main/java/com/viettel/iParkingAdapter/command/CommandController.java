d63d7c6f7f5199ff357d1d1f68799b0863bc76f0
kid <phamduy.itbk@gmail.com>
2018-12-10 14:51:33 +0700

    1234

--- iParkingAdapter/src/main/java/com/viettel/iParkingAdapter/command/CommandController.java
+++ iParkingAdapter/src/main/java/com/viettel/iParkingAdapter/command/CommandController.java
@@ -2,6 +2,7 @@ package com.viettel.iParkingAdapter.command;
 
 import com.viettel.iParkingAdapter.message.*;
 import com.viettel.iParkingAdapter.utils.TCPChannelManager;
+import io.netty.channel.ChannelHandlerContext;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
@@ -16,10 +17,10 @@ import io.netty.channel.Channel;
 public class CommandController {
     private Logger logger = LogManager.getLogger(this.getClass());
     public void responseDevice(OriginalMessage responseMsg){
-        Channel channel = TCPChannelManager.getChannels().get(responseMsg.getTerminalId());
+        ChannelHandlerContext channel = TCPChannelManager.getChannels().get(responseMsg.getTerminalId());
         if(channel != null){
             logger.info("send response into device id = " + responseMsg.getTerminalId());
-            if(channel.isActive() && channel.isWritable()){
+            if(channel.channel().isActive() && channel.channel().isWritable()){
                 channel.writeAndFlush(responseMsg);
             }else{
                 logger.error("channel is not active, can not send to device id = " + responseMsg.getTerminalId());
